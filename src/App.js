b3a05d29890041a9ff95618b1887379925b1b01c
Carlo Costantino <costantinoart@gmail.com>
2019-11-13 12:17:32 -1000

    Updated App.js to use Tabs component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import Messages from './Messages'
 import './App.css';
-// import TheDate from './state/TheDate';
-// import Counter from './state/Counter';
-import HelloWorld from './state-drills/HelloWorld';
-import Bomb from './state-drills/Bomb';
-import RouletteGun from './state-drills/RouletteGun';
+import Tabs from './state/Tabs.js';
+
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
 
 class App extends Component {
   render() {
     return (
       <div>
-        <HelloWorld />
-        <Bomb />
-        <RouletteGun />
-        <div>
-          <Messages name="Messages" unread={0}/>
-          <Messages name="Notifications" unread={10}/>
-        </div>
-        {/* <TheDate />
-        <Counter step={10}/> */}
+        <Tabs tabs={tabsProp} />
       </div>
     );
   }
